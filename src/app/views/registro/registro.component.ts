5f63ac26fbba40a59ceb67a12ee5f3aa5f187ba7
gonzalocruz <45164996+gonzalocruz@users.noreply.github.com>
2019-07-10 13:53:22 +0200

    Registro usuarios

--- src/app/views/registro/registro.component.ts
+++ src/app/views/registro/registro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Subject } from 'rxjs';
 import { filter, map, takeUntil } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
