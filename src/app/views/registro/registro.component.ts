515cadf6e944cd5f008ad7db09a4249cdec4ca74
gonzalocruz <45164996+gonzalocruz@users.noreply.github.com>
2019-07-10 12:10:40 +0200

    Login

--- src/app/views/registro/registro.component.ts
+++ src/app/views/registro/registro.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
+
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss']
+  styleUrls: ['./registro.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegistroComponent implements OnInit {
 
