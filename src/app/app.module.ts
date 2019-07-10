7b8694e7660d8ea14dc1b2b943e34dd68e682ea0
Ricardo Bautista <ricardo.bautistar@gmail.com>
2019-07-10 09:44:35 +0200

    Ricardo. Estructura creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegistroComponent } from './views/registro/registro.component';
+import { AltaComponent } from './views/alta/alta.component';
+import { StatusComponent } from './views/status/status.component';
+import { ConfirmComponent } from './views/confirm/confirm.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    AltaComponent,
+    StatusComponent,
+    ConfirmComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
