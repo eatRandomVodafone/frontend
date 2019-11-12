bb540c2f651b939e21c7773d8e36f1df34fdced1
Edu Iglesias <eduiglesias@47-60-25-195.red-acceso.airtel.net>
2019-11-12 16:09:23 +0100

    Delete folder component

--- src/app/guards/token.guard.ts
+++ src/app/guards/token.guard.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route, CanLoad } from '@angular/router';
-import { Observable } from 'rxjs';
-import { TokenService } from '../services/token.service';
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route, CanLoad} from '@angular/router';
+import {Observable} from 'rxjs';
+import {TokenService} from '../services/token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +9,16 @@ import { TokenService } from '../services/token.service';
 export class TokenGuard implements CanLoad {
 
   constructor(private router: Router,
-              private tokenSrv: TokenService ){
+              private tokenSrv: TokenService) {
 
   }
 
- async canLoad(route: Route) {
+  async canLoad(route: Route) {
     const token = await this.tokenSrv.getToken();
-    if(token){
+    if (token) {
       return Promise.resolve(true);
     }
-    this.router.navigate(['/login'])
+    this.router.navigate(['/login']);
     return Promise.resolve(true);
-
- }
-
-
+  }
 }
