33e75fcfa32f666af82126065e260c0df62d60c4
Edu Iglesias <eiglesiash@gmail.com>
2020-02-01 20:18:01 +0100

    Fuera la dependencia del proyecto de ws2reboot

--- src/app/interceptors/tokenInt.ts
+++ src/app/interceptors/tokenInt.ts
@@ -1,14 +1,14 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {TokenService} from '../services/token.service';
-import {Observable} from 'rxjs';
-import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { TokenService } from '../services/token.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
   constructor(private tokenSrv: TokenService,
-              private router: Router) {
+    private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -21,12 +21,11 @@ export class TokenInterceptor implements HttpInterceptor {
       Authorization: `Bearer ${jwt}`,
       crossOrigin: 'false'
     });
-    const reqClone = req.clone({headers});
+    const reqClone = req.clone({ headers });
 
     console.log('Peticion: ', reqClone);
 
     return next.handle(reqClone);
 
   }
-
 }
